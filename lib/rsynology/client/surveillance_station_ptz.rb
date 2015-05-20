5f3983af5be539b6992f9c58693809490118e84e
Kevin Southworth <kevin.southworth@webascender.com>
2015-05-20 09:40:26 -0400

    restore PTZ class

--- lib/rsynology/client/surveillance_station_ptz.rb
+++ lib/rsynology/client/surveillance_station_ptz.rb
@@ -1,36 +1,37 @@
-# module RSynology
-#   class Client
-#     class SurveillanceStationPtz < API
 
-#       DIRECTION = {
-#         right: "dir_0",
-#         right_up: "dir_1",
-#         up: "dir_2",
-#         left_up: "dir_3",
-#         left: "dir_4",
-#         left_down: "dir_5",
-#         down: "dir_6",
-#         right_down: "dir_7"
-#       }
+module RSynology
+  class Client
+    class SurveillanceStationPtz < API
 
-#       def self.api_name
-#         'SYNO.SurveillanceStation.PTZ'
-#       end
+      DIRECTION = {
+        right: "dir_0",
+        right_up: "dir_1",
+        up: "dir_2",
+        left_up: "dir_3",
+        left: "dir_4",
+        left_down: "dir_5",
+        down: "dir_6",
+        right_down: "dir_7"
+      }
 
-#       def move_url(camera_id, direction)
-#         params = {
-#           method: "Move",
-#           version: @maxVersion,
-#           api: api_name,
-#           _sid: @client.session_id,
-#           cameraId: camera_id,
-#           direction: direction,
-#           speed: 5 # fast
-#         }
+      def self.api_name
+        'SYNO.SurveillanceStation.PTZ'
+      end
 
-#         "#{@client.connection.url_prefix.to_s.chomp('/')}/webapi/#{@endpoint}?#{params.to_query}"
-#       end
+      def move_url(camera_id, direction)
+        params = {
+          method: "Move",
+          version: @maxVersion,
+          api: api_name,
+          _sid: @client.session_id,
+          cameraId: camera_id,
+          direction: direction,
+          speed: 5 # fast
+        }
 
-#     end
-#   end
-# end
+        "#{@client.connection.url_prefix.to_s.chomp('/')}/webapi/#{@endpoint}?#{params.to_query}"
+      end
+
+    end
+  end
+end
